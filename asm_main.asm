8ac2dd055f1b17241d869d0c39441cd53ceb4a10
angel acosta <angelacosta@cis.vvc.edu>
2017-11-16 16:01:39 -0800

    update

--- asm_main.asm
+++ asm_main.asm
@@ -1,22 +1,20 @@
-;
-; file: asm_main.asm
-
+; 
+; File: asm_main.asm
+; Name: Angel Acosta
+; Email: acostaa126247@student.vvc.edu
+; Description: Multiply each element in the array.
 %include "asm_io.inc"
 ;
 ; initialized data is put in the .data segment
 ;
 segment .data
-        syswrite: equ 4
-        stdout: equ 1
-        exit: equ 1
-        SUCCESS: equ 0
-        kernelcall: equ 80h
 
-array: dw 1,2,3,4,5
+
+array dw 1,2,3,4,5
+
 ; uninitialized data is put in the .bss segment
 ;
 segment .bss
-
 
 ;
 ; code is put in the .text segment
@@ -28,36 +26,24 @@ asm_main:
         pusha
 ; *********** Start  Assignment Code *******************
 
-	
 
-
-
-	mov eax, array
-	mov ebx, [eax]
-	push ebp
-	mov ebp, esp
-	
-
-
-FIRST:
-
-
-SEC:
-	inc edi
-		
-
-
-
-
-
-
-end:
-
+	xor eax, eax
+	mov dl, 5
+	mov ebx, array
+	mov ecx, 5
+LOOP1:
+	mov al, [ebx]
+	imul dl
+	call print_int
+	call print_nl
+	add ebx, 2
+	loop LOOP1
+NEXT:
 
 ; *********** End Assignment Code **********************
 
         popa
-        mov     eax, SUCCESS       ; return back to the C program
+        mov     eax, 0       ; return back to the C program
         leave                     
         ret
 
